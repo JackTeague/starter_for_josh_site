3fbc35ac9c0e7711fc4114f025389b70e0341b65
JackTeague <80437836+JackTeague@users.noreply.github.com>
2021-03-15 12:38:49 +0800

    Create Footer.php

---
+++ Footer.php
@@ -0,0 +1,18 @@
+<footer class="container-fluid">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 text-center footer-content">
+<p> <?php bloginfo('name'); ?></p>
+                <img src=" http://206.189.45.97/~mesh22/sub1/wp-content/uploads/2021/03/t.png" alt=“tumblr” >
+                <img src=" http://206.189.45.97/~mesh22/sub1/wp-content/uploads/2021/03/wifi.png" alt=“rss” >
+            </div>
+            <div class="col-md-6 text-center footer-content">
+                <p>Terms and Conditions</p>
+                <img src=" http://206.189.45.97/~mesh22/sub1/wp-content/uploads/2021/03/linkedin.png" alt=“linkedin” >
+                <img src=" http://206.189.45.97/~mesh22/sub1/wp-content/uploads/2021/03/Facebook.png" alt=“facebook” >
+            </div>
+
+        </div>
+    </div>
+</footer>
+<?php wp_footer(); ?>
