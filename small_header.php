f4841477837127b346241f207c18a2fdb04c1a6e
JackTeague <80437836+JackTeague@users.noreply.github.com>
2021-05-28 22:18:28 +0800

    Create small_header.php

---
+++ small_header.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/_/img/favicon.ico">
+
+    <title><?php
+		      if (function_exists('is_tag') && is_tag()) {
+		         single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
+		      elseif (is_archive()) {
+		         wp_title(''); echo ' Archive - '; }
+		      elseif (is_search()) {
+		         echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
+		      elseif (!(is_404()) && (is_single()) || (is_page())) {
+		         wp_title(''); echo ' - '; }
+		      elseif (is_404()) {
+		         echo 'Not Found - '; }
+		      if (is_home()) {
+		         bloginfo('name'); echo ' - '; bloginfo('description'); }
+		      else {
+		          bloginfo('name'); }
+		      if ($paged>1) {
+		         echo ' - page '. $paged; }
+           ?>
+    </title>
+
+
+
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;800&display=swap" rel="stylesheet">
+    <?php wp_head(); ?>
+
+</head>
+
+
+<body>
+    <header class="container-fluid small-herobg">
+        <nav class="navbar">
+            <div class="container-fluid ">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="josh orange">WHITKIN</span>
+                    </a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="active"><a href="#">Home</a></li>
+
+                        <li><a href="#">HOME</a></li>
+
+                        <li><a href="#">PROJECTS</a></li>
+
+                        <li><a href="#">PUBLICATIONS</a></li>
+
+                        <li><a href="#">CV</a></li>
+
+                        <li><a href="#">CONTACT</a></li>
+
+                    </ul>
+
+                </div>
+
+            </div>
+
+        </nav>
+
+        <div class="container">
+            <h1> Games Design for the future</h1>
+        </div>
+    </header>
