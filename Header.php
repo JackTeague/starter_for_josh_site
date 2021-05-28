b3b98456abe1bf6b5d0bc4a6dc71458228f62f63
JackTeague <80437836+JackTeague@users.noreply.github.com>
2021-05-28 12:01:37 +0800

    Update Header.php

--- Header.php
+++ Header.php
@@ -2,9 +2,6 @@
 <html lang="en">
   <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
       <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/_/img/favicon.ico">
       
 <title><?php
@@ -27,21 +24,12 @@
            ?>
 </title>
 
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
  
     
       
       <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;800&display=swap" rel="stylesheet">
-<?php wp_head(); ?> <!—very important that this is added-->
+<?php wp_head(); ?>
       
     </head>
     
